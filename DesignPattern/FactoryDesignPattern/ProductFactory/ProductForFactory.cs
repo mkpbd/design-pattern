dd38f7f4a83c492bd11025a1361f7212ff168862
kamalpassa <emsl.mostofakamal@gmail.com>
2023-10-04 12:22:52 +0600

    Factory Class

---
+++ DesignPattern/FactoryDesignPattern/ProductFactory/ProductForFactory.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryDesignPattern.ProductFactory
+{
+    /**
+     * 
+     * Factory design pattern একটি সফটওয়্যার ডিজাইন প্যাটার্ন যা ওপেন ক্লোজড প্রিন্সিপল অনুসরণ করে। 
+     * এটি এমন একটি ডিজাইন প্যাটার্ন যা নতুন অবজেক্টগুলির তৈরি প্রক্রিয়াটি একটি স্যাবক্লাস থেকে বিচ্যুত করে এবং অবজেক্টগুলির প্রক্রিয়াটি সাবক্লাস দিয়ে বিশেষভাবে মান্যতা দেয়।
+     * 
+     * ProductFactory ক্লাস একটি ফ্যাক্টরি ক্লাস যা নতুন প্রোডাক্ট তৈরির জন্য CreateProduct মেথড সরবরাহ করে। 
+     * মূল Product ক্লাসের নতুন অবজেক্টগুলি এই ফ্যাক্টরি ক্লাসের মাধ্যমে তৈরি হয় এবং প্রোডাক্টের নাম এবং মূল্য সেট করা হয়।
+     * 
+     * 
+     */
+    public class ProductForFactory
+    {
+        // একটি ফ্যাক্টরি মেথড যা নতুন প্রোডাক্ট তৈরি করে ফেরত দেয়
+        public Product CreateProduct(string name, double price)
+        {
+            Product product = new Product
+            {
+                Name = name,
+                Price = price
+            };
+            return product;
+        }
+    }
+}
--- DesignPattern/FactoryDesignPattern/Program.cs
+++ DesignPattern/FactoryDesignPattern/Program.cs
@@ -1,4 +1,5 @@
 ﻿using FactoryDesignPattern.FactoryClasses;
+using FactoryDesignPattern.ProductFactory;
 
 namespace FactoryDesignPattern
 {
@@ -11,6 +12,16 @@ namespace FactoryDesignPattern
            var platinum =  creditCard.GetCreditCard("Platinum");
 
             Console.WriteLine(platinum.GetCreditLimit());
+
+
+            // ফ্যাক্টরি ক্লাস থেকে নতুন প্রোডাক্ট তৈরি করতে পারেন
+            ProductForFactory factory = new ProductForFactory();
+            Product product1 = factory.CreateProduct("ল্যাপটপ", 800.00);
+            Product product2 = factory.CreateProduct("মোবাইল ফোন", 300.00);
+
+            // তৈরি প্রোডাক্টের তথ্য দেখানো
+            Console.WriteLine($"প্রোডাক্ট 1: {product1.Name}, মূল্য: ${product1.Price}");
+            Console.WriteLine($"প্রোডাক্ট 2: {product2.Name}, মূল্য: ${product2.Price}");
         }
     }
 }
