3388e3805651f527646411652f86cc401a7497ef
kamalpassa <emsl.mostofakamal@gmail.com>
2023-10-04 11:52:07 +0600

    Image and  defination

--- DesignPattern/FactoryDesignPattern/FactoryClasses/CreditCardFactory.cs
+++ DesignPattern/FactoryDesignPattern/FactoryClasses/CreditCardFactory.cs
@@ -7,8 +7,18 @@ using System.Threading.Tasks;
 
 namespace FactoryDesignPattern.FactoryClasses
 {
-    internal class CreditCardFactory
+    public class CreditCardFactory
     {
+        /**
+         * 
+         *  A factory is an object used for creating other objects. 
+         *  In technical terms, we can say that a factory is a class with a method. 
+         *  That method will create and return different objects based on the received input parameter.
+         * 
+         * In simple words, if we have a superclass and n number of subclasses, and based on the data provided, if we have to create and return the object of one of the subclasses, then we need to use the Factory Design Pattern in C#.
+         * 
+         */
+
 
         public ICreditCard GetCreditCard(string cardType)
         {
---
+++ DesignPattern/readme.md
@@ -0,0 +1,3 @@
+[Paste](https://file+.vscode-resource.vscode-cdn.net/f%3A/Jallal/design-pattern/DesignPattern/# "#")
+
+![1696398413354](image/readme/1696398413354.png)
